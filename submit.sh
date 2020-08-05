471635932763a608f08deebb9649992621a0a88e
Brendan Cron <brendancollege85@gmail.com>
2020-08-05 08:34:12 -0400

    fixed gitignore

---
+++ submit.sh
@@ -0,0 +1,10 @@
+git add code.py
+DATE=$(date)
+MESSAGE="Submission at: $DATE"
+git commit -m "$MESSAGE"
+git push origin master
+CODE_REPO="$(git config --get remote.origin.url)"
+TEST_REPO="https://github.com/brendancron/ExampleTest.git"
+DATA='{"code_repo":"'"$CODE_REPO"'","test_repo":"'"$TEST_REPO"'"}'
+echo $DATA
+curl -H 'Content-Type: application/json' -d $DATA http://localhost:5000/submit
