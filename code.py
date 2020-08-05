d0cf92a66872ee3dd9542bb2495a63e29884906f
Brendan Cron <brendancollege85@gmail.com>
2020-08-04 22:12:44 -0400

    Submission at: Tue Aug  4 22:12:44 EDT 2020

--- code.py
+++ code.py
@@ -1,17 +1,17 @@
 def add(x, y):
-    return x+x
+    return x+y
 
 def multiply(x, y):
-    return x*x
+    return x*y
 
 def subtract(x, y):
-    return x-x
+    return x-y
 
 
 def divide(x, y):
     if y==0:
         raise ValueError("Can't divide by zero")
-    return x/x
+    return x/y
 
 if (__name__ == "__main__"):
     print('i am main')
